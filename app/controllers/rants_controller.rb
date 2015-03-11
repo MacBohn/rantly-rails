2e772d311e38f10cb64e624930e0b775dc7f4ccf
McCartney Bohn <mccartneybohn@yahoo.com>
2015-03-11 13:18:19 -0600

    added registrations controller and view and users controller and view and rants controllers and views

---
+++ app/controllers/rants_controller.rb
@@ -0,0 +1,7 @@
+class RantsController < ApplicationController
+
+  def index
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pages#index'
+  root 'rants#index'
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
