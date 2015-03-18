746343d5bf43fe4900c75311c2042d1a439fb413
McCartney Bohn <mccartneybohn@yahoo.com>
2015-03-18 15:09:05 -0600

    users can only edit thier own rants added spec

--- spec/features/rants_spec.rb
+++ spec/features/rants_spec.rb
@@ -84,4 +84,37 @@ feature 'Rants' do
         expect(page).to have_content("Body is too short (minimum is 144 characters)")
     end
 
+    scenario "Users can't edit a rant" do
+      visit '/sign-up'
+      fill_in "First name", with: "Mac"
+      fill_in "Last name", with: "Bohn"
+      fill_in "Email", with: "Mac@Bohn.com"
+      fill_in "Password", with: "password"
+      within("#new_user") do
+        click_on "Sign Up"
+      end
+
+      click_on "New Rant"
+      fill_in "Title", with: "This is the title"
+      fill_in "Body", with: ("This is the body" * 10)
+      click_on "Save"
+      expect(page).to have_content("This is the title")
+      expect(page).to have_content("This is the body")
+        visit root_path
+
+        click_on "Sign Out"
+
+        visit '/sign-up'
+        fill_in "First name", with: "John"
+        fill_in "Last name", with: "Jones"
+        fill_in "Email", with: "John@Jones.com"
+        fill_in "Password", with: "password"
+        within("#new_user") do
+          click_on "Sign Up"
+        end
+        visit root_path
+        expect(page).to_not have_content("Edit")
+
+      end
+
 end
