21841074ee7070f883aff7ac514733fbe5e4c3c2
McCartney Bohn <mccartneybohn@yahoo.com>
2015-03-30 09:28:09 -0600

    added jquery for the cancel buttom

--- app/assets/javascripts/count.js
+++ app/assets/javascripts/count.js
@@ -1,10 +1,17 @@
+
 function updateCountdown() {
-    var remaining = jQuery('.message').val().length;
-    jQuery('.countdown').text(remaining + " Characters");
+  var remaining = jQuery('.message').val().length;
+  jQuery('.countdown').text(remaining + " Characters");
 }
 
 jQuery(document).ready(function($) {
-    updateCountdown();
-    $('.message').change(updateCountdown);
-    $('.message').keyup(updateCountdown);
+  updateCountdown();
+  $('.message').change(updateCountdown);
+  $('.message').keyup(updateCountdown);
+
+  $(".cancel-button").click(function (e){
+    e.preventDefault();
+    $(".rant-box").fadeOut('slow');
+  });
+
 });
