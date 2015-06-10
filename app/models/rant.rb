99df832c90f1791f774e5bc7083229df9bf25eb2
McCartney Bohn <mccartneybohn@yahoo.com>
2015-06-09 20:02:28 -0600

    fixed form group error

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -3,7 +3,7 @@ class Rant < ActiveRecord::Base
   belongs_to :user
 
   validates_length_of :body, :minimum => 144, :allow_blank => false
-
+  validates :body, presence: :true
   validates :title, presence: :true
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ActiveRecord::Base
 
   include Gravtastic
     gravtastic
-    
+
   has_secure_password
+
+  validates :email, presence: :true
+  validates :password, presence: :true
+
 
   def full_name
     "#{first_name}  #{last_name}"
